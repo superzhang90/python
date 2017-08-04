0b91f44f86c146f47609cea5ac67458134546ec4
Charley Zhang <super_zc90@163.com>
2017-08-04 16:29:51 +0800

    planegame

---
+++ WeChat_planegame/supply.py
@@ -0,0 +1,61 @@
+# 导入相关模块
+# coding: utf-8
+# ===============================================================================
+# ��Ҫ���ܣ�����������Ų����������������ȫ��ը�����߳����ӵ���
+# �㷨���̣�1��
+# ע�����1����֮ǰ������Ķ���ܹ�����
+# ===============================================================================
+# �������ģ��
+# coding: utf-8
+import pygame
+from random import *
+
+
+# ====================���峬���ӵ�������====================
+class BulletSupply(pygame.sprite.Sprite):
+    def __init__(self, bg_size):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load("image/ufo1.png")
+        self.rect = self.image.get_rect()
+        self.width, self.height = bg_size[0], bg_size[1]
+        self.rect.left, self.rect.bottom = randint(0, self.width - self.rect.width), -100
+        self.speed = 5
+        self.active = False
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def move(self):
+        if self.rect.top < self.height:
+            self.rect.top += self.speed
+        else:
+            self.active = False
+
+    def reset(self):
+        self.active = True
+        self.rect.left, self.rect.bottom = randint(0, self.width - self.rect.width), -100
+
+
+# ====================���峬��ը��������====================
+class BombSupply(pygame.sprite.Sprite):
+    def __init__(self, bg_size):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load("image/ufo2.png")
+        self.rect = self.image.get_rect()
+        self.width, self.height = bg_size[0], bg_size[1]
+        self.rect.left, self.rect.bottom = randint(0, self.width - self.rect.width), -100
+        self.speed = 5
+        self.active = False
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def move(self):
+        if self.rect.top < self.height:
+            self.rect.top += self.speed
+        else:
+            self.active = False
+
+    def reset(self):
+        self.active = True
+        self.rect.left, self.rect.bottom = randint(0, self.width - self.rect.width), -100
+
+
